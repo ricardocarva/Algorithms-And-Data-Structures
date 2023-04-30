add7fecf97c77e96094b3f9e4a1f16c1a2c5de0f
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-04-30 11:14:16 -0300

    Add Tower of Hanoi in Clojure

---
+++ src/clojure/tower-of-hanoi.clj
@@ -0,0 +1,16 @@
+(ns clojure.tower_of_hanoi)
+
+(defn hanoi
+  ([disks] (hanoi [] 0 2 1 disks))
+  ([acc pin0 pin2 pin1 disks]
+  (cond
+    (= disks 1) (conj acc {:from pin0 :to pin2})
+    :else (into [] (concat
+            (hanoi acc pin0 pin1 pin2 (- disks 1))
+            (hanoi acc pin0 pin2 pin1 1)
+            (hanoi acc pin1 pin2 pin0 (- disks 1)))))))
+
+(->> (hanoi 3)
+     (map #(str "Move from " (:from %) " to " (:to %)))
+     (clojure.string/join "\n")
+     println)
